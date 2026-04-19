d98676e135e9ba748d2e7a5a71a2831ea18f35c2
ivargek23 <ivargek@algebra.hr>
2026-04-19 19:37:39 +0200

    Initial commit

---
+++ ApacheHW/src/main/java/hr/algebra/apachehw/ApacheHwApplication.java
@@ -0,0 +1,13 @@
+package hr.algebra.apachehw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApacheHwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApacheHwApplication.class, args);
+    }
+
+}
